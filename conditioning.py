cb9af2a6c44665df3da2c8fc4054a65c17400a66
bryankim96 <bryan.sanghyuk.kim@gmail.com>
2018-03-07 12:32:07 -0500

    Wrote function to return conditioning vector from embedding.

---
+++ conditioning.py
@@ -0,0 +1,23 @@
+
+import tensorflow as tf
+
+def get_conditioning_vector(embedding,conditioning_vector_size=128):
+
+    # compute mean and stddev from the embedding using a fully connected layer with
+    # leaky relu activation
+    conditioning_dist_params = tf.layers.dense(embedding, units=conditioning_vector_size*2,activation=tf.nn.leaky_relu)
+
+    mu = conditioning_dist_params[:,conditioning_vector_size:]
+    log_sigma = conditioning_dist_params[:,:conditioning_vector_size]
+    sigma = tf.exp(log_sigma)
+
+    # create an embedding_size-dimensional gaussian distribution 
+    # with the provided mean and stddevs 
+    
+    # consider using truncated normal?
+    dist = tf.contrib.distributions.MultivariateNormalDiag(loc=mu, scale=sigma)
+
+    # sample from the distribution to get the conditioning vector
+    conditioning_vector = dist.sample()
+
+    return conditioning_vector
