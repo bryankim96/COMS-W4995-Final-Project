d112a43dd6b1488fe50b0a9866b82dd79a092703
Bryan Kim <bryan.sanghyuk.kim@gmail.com>
2018-04-02 23:53:39 +0000

    Implemented eval image saving.

--- train.py
+++ train.py
@@ -2,8 +2,11 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
-
+from PIL import Image, ImageDraw
+import scipy
+ 
 import argparse
+import random
 
 tfgan = tf.contrib.gan
 
@@ -11,7 +14,12 @@ from conditioning import get_conditioning_vector
 from stage_1 import generator_stage1, discriminator_stage1
 from stage_2 import generator_stage2, discriminator_stage2
 
+from misc.preprocess_birds import LOAD_SIZE, LR_HR_RATIO, load_bbox
+from misc.utils import get_image
+
 BATCH_SIZE = 64
+
+NUM_EVAL = 10
 
 Z_DIM = 100
 EMBEDDING_DIM = 128
@@ -122,33 +130,32 @@ if __name__=="__main__":
         # regularization term in the generator loss
         batch_conditioning_vectors, kl_div = get_conditioning_vector(batch_embeddings, conditioning_vector_size=EMBEDDING_DIM)
 
-        with tf.variable_scope('model'):
-            model = tfgan.gan_model(
-                generator_fn=generator_function,
-                discriminator_fn=discriminator_function,
-                real_data=batch_images,
-                generator_inputs=(batch_z, batch_conditioning_vectors, kl_div))
+        model = tfgan.gan_model(
+            generator_fn=generator_function,
+            discriminator_fn=discriminator_function,
+            real_data=batch_images,
+            generator_inputs=(batch_z, batch_conditioning_vectors, kl_div))
 
-            loss = tfgan.gan_loss(model,
-                    generator_loss_fn=generator_loss_function,
-                    discriminator_loss_fn=discriminator_loss_function)
+        loss = tfgan.gan_loss(model,
+                generator_loss_fn=generator_loss_function,
+                discriminator_loss_fn=discriminator_loss_function)
 
-            generator_optimizer = tf.train.AdamOptimizer(0.002, beta1=0.5)
-            discriminator_optimizer = tf.train.AdamOptimizer(0.0002, beta1=0.5)
+        generator_optimizer = tf.train.AdamOptimizer(0.002, beta1=0.5)
+        discriminator_optimizer = tf.train.AdamOptimizer(0.0002, beta1=0.5)
 
-            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-            with tf.control_dependencies(update_ops):
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
 
-                gan_train_ops = tfgan.gan_train_ops(model, loss, generator_optimizer, discriminator_optimizer)
+            gan_train_ops = tfgan.gan_train_ops(model, loss, generator_optimizer, discriminator_optimizer)
 
-                global_step = tf.train.get_or_create_global_step()
-                train_step_fn = tfgan.get_sequential_train_steps()
+            global_step = tf.train.get_or_create_global_step()
+            train_step_fn = tfgan.get_sequential_train_steps()
 
-            # set up image summaries
-            tf.summary.image('real_images', batch_images)
-            tf.summary.image('generated_images', model.generated_data)
-            summary_op = tf.summary.merge_all()
-            summary_hook = tf.train.SummarySaverHook(save_secs=300,output_dir=args.logdir,summary_op=summary_op)
+        # set up image summaries
+        tf.summary.image('real_images', batch_images)
+        tf.summary.image('generated_images', model.generated_data)
+        summary_op = tf.summary.merge_all()
+        summary_hook = tf.train.SummarySaverHook(save_secs=300,output_dir=args.logdir,summary_op=summary_op)
 
         with tf.train.MonitoredTrainingSession(hooks=[summary_hook], checkpoint_dir=args.logdir) as sess:
             sess.run(iterator.initializer)
@@ -159,40 +166,80 @@ if __name__=="__main__":
                 for i in range(NUM_STEPS):
                     cur_loss, _ = train_step_fn(sess, gan_train_ops, global_step, train_step_kwargs={})
 
-    elif args.mode == 'predict':
+    elif args.mode == 'eval':
 
-        test_filenames = [TEST_DIR + '/data.tfrecord']
-        dataset = tf.data.TFRecordDataset(test_filenames)
-        dataset = dataset.map(_parse_function, num_parallel_calls=4)
-        dataset = dataset.repeat()
-        dataset = dataset.batch(BATCH_SIZE)
-        iterator = dataset.make_initializable_iterator()
+        # get class ids as list of len (2933)
+        with open(TEST_DIR + '/class_info.pickle', 'rb') as f:
+            class_ids = pickle.load(f)
 
-        batch_images, batch_embeddings = iterator.get_next()
+        # load embeddings as numpy array of shape (2933, 10, 1024)
+        with open(TEST_DIR + '/char-CNN-RNN-embeddings.pickle', 'rb') as f:
+            embeddings = pickle.load(f,encoding='latin1')
+            embeddings = np.array(embeddings)
 
+        # get list of filenames of len (2933)
+        with open(TEST_DIR + '/filenames.pickle', 'rb') as f:
+            list_filenames = pickle.load(f)
+            
+        # choose a random test image filename
+        index, filename = random.choice(list(enumerate(list_filenames)))	
+
+        # get the corresponding captions
+        with open(DATA_DIR + '/text_c10/' + filename + '.txt', "r") as f:
+            captions = f.read().split('\n')
+        captions = [cap for cap in captions if len(cap) > 0]
+	
+        # randomly select 1 caption and the corresponding embedding
+        j, caption = random.choice(list(enumerate(captions)))
+        embedding = embeddings[index][j]
+
+        # load the example true image (process as usual down to 76 x 76
+        lr_size = int(LOAD_SIZE / LR_HR_RATIO)
+        filename_bbox = load_bbox('Data/birds/')
+        bbox = filename_bbox[filename]
+        f_name = 'Data/birds/CUB_200_2011/images/%s.jpg' % filename
+        img = get_image(f_name, LOAD_SIZE, is_crop=True, bbox=bbox) 
+        img = img.astype(np.float32)
+        true_img = scipy.misc.imresize(img, [lr_size, lr_size], 'bicubic').astype(np.float32)
+        
+        # convert the embedding to a tensor and repeat BATCH_SIZE times to shape (BATCH_SIZE, 1024)
+        embedding = tf.constant(embedding)
+        batch_embeddings =  tf.tile(tf.expand_dims(embedding, axis=0),[BATCH_SIZE,1])
+       
+        batch_z = tf.random_normal([BATCH_SIZE, Z_DIM])
+        
         # get conditioning vector (from embedding) and KL divergence for use as a
         # regularization term in the generator loss
+        # get batch of conditioning vectors of shape (NUM_EVAL, EMBEDDING_DIM)
         batch_conditioning_vectors, kl_div = get_conditioning_vector(batch_embeddings, conditioning_vector_size=EMBEDDING_DIM)
 
-        model = tfgan.gan_model(
-            generator_fn=generator_function,
-            discriminator_fn=discriminator_function,
-            real_data=batch_images,
-            generator_inputs=(batch_z, batch_conditioning_vectors, kl_div))
+        with tf.variable_scope('Generator'):
+            eval_images = generator_function((batch_z, batch_conditioning_vectors, kl_div), is_training=False)
+        reshaped_eval_imgs = tfgan.eval.image_reshaper(eval_images[:NUM_EVAL,:,:,:], num_cols=NUM_EVAL)
 
         saver = tf.train.Saver()
 
-        eval_images = gan_model.generator_fn((batch_z, batch_conditioning_vectors, kl_div), is_training=False)
-        reshaped_eval_imgs = tfgan.eval.image_reshaper(eval_images[:20, ...], num_cols=10)
-
         with tf.Session() as sess:
-            # Restore variables from disk.
-            saver.restore(sess, os.path.join(args.logdir, "model.ckpt")
+            saver.restore(sess,tf.train.latest_checkpoint(args.logdir))
+            # tf.contrib.framework.init_from_checkpoint(args.logdir,{'Generator/': 'Generator/'})
+           
+            # get out BATCH_SIZE predicted images 
+            eval_images_array = sess.run(reshaped_eval_imgs)
 
-            eval_images = sess.run([reshaped_eval_imgs])
+        # draw composite image with PIL
+        # display true image, NUM_EVAL example generated images, and caption
+        # in a single figure
+        composite_img = Image.new('RGB', (NUM_EVAL*64 + 200 + 76, 200))
 
-            plt.axis('off')
-            plt.imsave('test_images.png', eval_images)
+        d = ImageDraw.Draw(composite_img)
+        d.text((20,30), caption, fill=(255,255,0))
+ 
+        true_img = Image.fromarray(true_img.astype(np.uint8))    
+        composite_img.paste(true_img,(50,112))
+        eval_images_array = Image.fromarray(((eval_images_array[0] + 1.0) * (255.0 / 2.0)).astype(np.uint8))
+        composite_img.paste(eval_images_array,(176,118))
+
+        composite_img.save('eval.png')
 
     else:
         raise ValueError('Invalid mode.')
